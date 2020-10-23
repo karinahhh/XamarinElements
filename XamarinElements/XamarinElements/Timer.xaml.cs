d6c6df8809452428da7a40c48131612f8f8ff44a
opilane <opilane@HP-CZC6297BB1>
2020-10-23 09:54:07 +0300

    timer+datapicker

--- XamarinElements/XamarinElements/Timer.xaml.cs
+++ XamarinElements/XamarinElements/Timer.xaml.cs
@@ -12,9 +12,39 @@ namespace XamarinElements
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Timer : ContentPage
 	{
-		public Timer()
-		{
-			InitializeComponent();
-		}
-	}
+        Button timerButton;
+        bool alive = true;
+        public Timer()
+        {
+            timerButton = new Button
+            {
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center,
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button))
+            };
+            timerButton.Clicked += TimerButton_Clicked;
+
+            Content = timerButton;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+        }
+        private bool OnTimerTick()
+        {
+            timerButton.Text = DateTime.Now.ToString("T");
+            return alive;
+        }
+
+        private void TimerButton_Clicked(object sender, EventArgs e)
+        {
+            if (alive == true)
+            {
+                alive = false;
+            }
+            else
+            {
+                alive = true;
+                Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+            }
+        }
+    }
 }
--- XamarinElements/XamarinElements/dPicker.xaml.cs
+++ XamarinElements/XamarinElements/dPicker.xaml.cs
@@ -12,9 +12,23 @@ namespace XamarinElements
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class dPicker : ContentPage
 	{
+		Label lbl;
+		DatePicker date;
 		public dPicker()
 		{
-			InitializeComponent();
+			lbl = new Label { Text = "Выбери дату" };
+			date = new DatePicker();
+			date.Format = "D";
+			//date.MinimumDate = DateTime.Now.AddDays(5);
+			//date.MaximumDate = DateTime.Now.AddDays(-5);
+			date.DateSelected += Date_DateSelected;
+			StackLayout stack = new StackLayout { Children = { lbl, date } };
+			this.Content = stack;
+		}
+
+		private void Date_DateSelected(object sender, DateChangedEventArgs e)
+		{
+			lbl.Text = "Вы выбрали " + e.NewDate.ToString("dd/MM/yyyy");
 		}
 	}
 }
